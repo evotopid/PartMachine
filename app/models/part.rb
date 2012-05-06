aa131b64ecc698e62350108ba9df0e94f7ebf0e6
Evotopid <evotopid@gmail.com>
2012-05-06 13:08:45 +0200

    part model now validates presence of name and description

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,7 @@
 class Part < ActiveRecord::Base
   attr_accessible :description, :name
   belongs_to :category
+  
+  validates :name,        presence: true
+  validates :description, presence: true
 end
