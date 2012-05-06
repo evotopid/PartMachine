beb448e6ed181d773bcd357a52449455a61c97cd
Evotopid <evotopid@gmail.com>
2012-05-06 12:55:45 +0200

    added has many to parts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120506104851) do
+ActiveRecord::Schema.define(:version => 20120506105043) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
