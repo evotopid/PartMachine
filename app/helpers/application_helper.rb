804d6d9f99281b16a35efbe0f30758bec200c38e
Evotopid <evotopid@gmail.com>
2012-05-06 14:58:14 +0200

    fixed title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title ( page_title )
-    if page_title.nil?
+    if page_title.nil? or page_title.empty?
       "PartMachine"
     else
       "PartMachine > #{page_title}"
