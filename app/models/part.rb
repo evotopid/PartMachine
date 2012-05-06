f9eb6d7c8dfaf8ab8844d4d0921ee7889787d4c6
Evotopid <evotopid@gmail.com>
2012-05-06 17:51:42 +0200

    integrated the amount into the application

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,7 +1,8 @@
 class Part < ActiveRecord::Base
-  attr_accessible :description, :name, :category_id
+  attr_accessible :description, :name, :amount, :category_id
   belongs_to :category
   
   validates :name,        presence: true
   validates :description, presence: true
+  validates :amount,      presence: true, numericality: { only_integer: true }
 end
