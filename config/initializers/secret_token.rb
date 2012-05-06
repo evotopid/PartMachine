75ab9a0de2468a2bd5cc9b91266d1ec0ccef98b3
Evotopid <evotopid@gmail.com>
2012-05-06 12:39:36 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartMachine::Application.config.secret_token = 'e1ef215103f3daa2ee7f72e6ad755912257aee97eb784e38ca6885b66d08823c83e574fbe6bae1259420fadd906b319c3b7d8d338485f69b984d8e94b2c4a884'
