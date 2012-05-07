ddcba4a9377461a9496a9542a4494e1802f4c23b
Evotopid <evotopid@gmail.com>
2012-05-07 16:33:35 +0200

    added sphinx indexes to part model

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,6 +2,11 @@ class Part < ActiveRecord::Base
   attr_accessible :description, :name, :amount, :category_id
   belongs_to :category
   
+  define_index do
+    indexes name, sortable: true
+    indexes description
+  end
+  
   validates :name,        presence: true
   validates :description, presence: true
   validates :amount,      presence: true, numericality: { only_integer: true }
