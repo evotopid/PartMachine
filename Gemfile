7110907d25fe0be5cfad6ab8e29fc8ff6cc60eef
Evotopid <evotopid@gmail.com>
2012-05-21 19:43:00 +0200

    don't use sphinx anymore

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
-gem 'thinking-sphinx', '2.0.12'
 
 group :assets do
   gem 'sass-rails',   '3.2.5'
--- app/models/part.rb
+++ app/models/part.rb
@@ -2,22 +2,7 @@ class Part < ActiveRecord::Base
   attr_accessible :description, :name, :amount, :category_id
   belongs_to :category
   
-  define_index do
-    indexes name, sortable: true
-    indexes description
-    
-    set_property :delta => true
-  end
-  
   validates :name,        presence: true
   validates :description, presence: true
   validates :amount,      presence: true, numericality: { only_integer: true }
-  
-  after_save :set_delta_flag
-  after_destroy :set_delta_flag
-  
-  def set_delta_flag
-    self.delta = true
-    #self.save
-  end
 end
