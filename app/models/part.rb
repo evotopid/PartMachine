d186665f3be298ebe986000549771979d95d317f
Evotopid <evotopid@gmail.com>
2012-05-21 17:47:41 +0200

    added some parts for sphinx

--- app/models/part.rb
+++ app/models/part.rb
@@ -5,9 +5,19 @@ class Part < ActiveRecord::Base
   define_index do
     indexes name, sortable: true
     indexes description
+    
+    set_property :delta => true
   end
   
   validates :name,        presence: true
   validates :description, presence: true
   validates :amount,      presence: true, numericality: { only_integer: true }
+  
+  after_save :set_delta_flag
+  after_destroy :set_delta_flag
+  
+  def set_delta_flag
+    self.delta = true
+    #self.save
+  end
 end
