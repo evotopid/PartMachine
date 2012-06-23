d67b6d78fc48fd5d9997abb4dbd7ce1cc9a08366
Evotopid <evotopid@gmail.com>
2012-06-23 16:48:11 +0200

    added custom entry in the footer for the categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :load_layout_data
+  
+  def load_layout_data
+    @categories = Category.all
+  end
 end
